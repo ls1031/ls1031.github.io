0f0559451824a883e6d464dc6539afb4aa16b1c2
ls1031 <125716107+ls1031@users.noreply.github.com>
2023-02-19 10:24:38 -0800

    Create CitationListCreator.dart

---
+++ CitationListCreator.dart
@@ -0,0 +1,12 @@
+import 'queryFunc.dart';
+import 'package:tuple/tuple.dart';
+
+Future<List<Tuple2<String, String>>> getListOfCitations(Future<Map<String, dynamic>> apiJsonResponse) async {
+  final tmp = (await apiJsonResponse)["results"];
+  List<Tuple2<String, String>> result = [];
+  for (int i = 0; i < tmp.length; ++i)
+    result.add(Tuple2<String, String>(tmp[i]["name_abbreviation"]
+      + " (${tmp[i]['decision_date'].substring(0, 4)})",
+      tmp[i]['frontend_pdf_url']));
+  return result;
+}
